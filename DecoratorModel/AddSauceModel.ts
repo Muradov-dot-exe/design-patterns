bf14fbe4f838a74fa87816bfd0b0471e3af8568c
Ali Muradov <amuradov239@gmail.com>
2023-05-25 17:01:43 +0300

    added decorator

---
+++ DecoratorModel/AddSauceModel.ts
@@ -0,0 +1,38 @@
+class BurgerDecorator implements Burger {
+  protected burger: Burger;
+
+  constructor(burger: Burger) {
+    this.burger = burger;
+  }
+  getBurgerType(): string {
+    return this.burger.getBurgerType();
+  }
+
+  public getDescription(): string {
+    return this.burger.getDescription();
+  }
+
+  public addSauce(sauce: string): void {
+    this.burger.addSauce(sauce);
+  }
+}
+
+class KetchupDecorator extends BurgerDecorator {
+  public addSauce(sauce: string): void {
+    if (sauce === "кетчуп") {
+      this.burger.addSauce(sauce);
+    } else {
+      console.log("Невъзможно добавяне на кетчуп към бургер!");
+    }
+  }
+}
+
+class GarlicSauceDecorator extends BurgerDecorator {
+  public addSauce(sauce: string): void {
+    if (sauce === "чеснов сос") {
+      this.burger.addSauce(sauce);
+    } else {
+      console.log("Невъзможно добавяне на чеснов сос към бургер!");
+    }
+  }
+}
