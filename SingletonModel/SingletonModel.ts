cbaf61cc1c1000ac5244feb36302b81c601dbfde
Ali Muradov <amuradov239@gmail.com>
2023-05-25 16:51:58 +0300

    created singleton

---
+++ SingletonModel/SingletonModel.ts
@@ -0,0 +1,19 @@
+class Restaurant {
+  private static instance: Restaurant;
+
+  private constructor() {}
+
+  public static getInstance(): Restaurant {
+    if (!Restaurant.instance) {
+      Restaurant.instance = new Restaurant();
+    }
+    return Restaurant.instance;
+  }
+
+  public takeOrder(order: Order): void {
+    const chef = Chef.getInstance();
+    const cashier = Cashier.getInstance();
+    const burger = chef.prepareBurger(order);
+    cashier.processOrder(burger);
+  }
+}
